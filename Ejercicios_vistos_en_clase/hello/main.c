c38efd9d69fd69c26538e90a944371a324e9eb26
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-13 00:03:00 -0600

    Hello world C

--- Ejercicios_vistos_en_clase/hello/main.c
+++ Ejercicios_vistos_en_clase/hello/main.c
@@ -1,6 +1,24 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void* run(void* data);
 
 int main(void){
-  printf("Hello from main thread\n");
-  return 0;
+  pthread_t thread; //a register is created to control the thread
+  if(pthread_create(&thread, /*attribute*/ NULL, run, (void*)13) == EXIT_SUCCESS){
+    printf("Hello from main thread\n");
+    pthread_join(thread, /*value_ptr*/ NULL);
+    return EXIT_SUCCESS;
+  }else{
+    fprintf(stderr,"could not create secundary thread\n");
+    return EXIT_FAILURE;
+  }
+
+}
+
+void* run(void* data){
+  printf("Hello from secundary thread\n");
+  printf("data = %p\n", data);
+  return NULL;
 }
