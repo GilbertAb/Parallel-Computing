09b1cdc374d1d2ea807162d090ebf28df8719ebd
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-13 10:51:13 -0600

    Makefile for Hello Pthreads

--- Ejercicios_vistos_en_clase/hello/main.c
+++ Ejercicios_vistos_en_clase/hello/main.c
@@ -1,23 +1,23 @@
+// Cpyright 2021 Gilbert Marquez Aldana <gilbert.marquez@ucr.ac.cr>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 void* run(void* data);
 
-int main(void){
-  pthread_t thread; //a register is created to control the thread
-  if(pthread_create(&thread, /*attribute*/ NULL, run, (void*)13) == EXIT_SUCCESS){
+int main(void) {
+  pthread_t thread;  // a register is created to control the thread
+  if( pthread_create( &thread, /*attribute*/ NULL, run, (void*)13 ) == EXIT_SUCCESS) {
     printf("Hello from main thread\n");
     pthread_join(thread, /*value_ptr*/ NULL);
     return EXIT_SUCCESS;
-  }else{
-    fprintf(stderr,"could not create secundary thread\n");
+  } else {
+    fprintf(stderr, "could not create secundary thread\n");
     return EXIT_FAILURE;
   }
-
 }
 
-void* run(void* data){
+void* run(void* data) {
   printf("Hello from secundary thread\n");
   printf("data = %p\n", data);
   return NULL;
