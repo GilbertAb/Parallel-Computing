a9d614dacf06d7f915c3f23ba0f4044128425bba
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-17 16:23:26 -0600

    Tests for goldbach_serial agregated

--- Tareas/goldbach_serial/goldbach_serial.c
+++ Tareas/goldbach_serial/goldbach_serial.c
@@ -1,7 +1,30 @@
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
+int64_t goldbach(int64_t * number);
 
+int main(void) {
+  FILE* input = stdin;
+  FILE* output = stdout;
+  int64_t number = 0;
+  
+  while(fscanf(input, "%"SCNd64, &number) == 1){
+    fprintf(output, "%"SCNd64"\n", goldbach(&number));
+  }
+  
   return EXIT_SUCCESS;
 }
+
+int64_t goldbach(int64_t * number) {
+  int64_t goldbach= *number + *number;
+  
+  //int * primesVector = calculatePrimeNumbers();
+  
+  //if(*number )
+
+
+  return goldbach;
+}
+
