87def2b6ef72c45a4aa195d16a8205f257206470
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-17 17:55:57 -0600

    goldbach_serial: main

--- Tareas/goldbach_serial/goldbach_serial.c
+++ Tareas/goldbach_serial/goldbach_serial.c
@@ -11,14 +11,20 @@ int main(void) {
   int64_t number = 0;
   
   while(fscanf(input, "%"SCNd64, &number) == 1){
-    fprintf(output, "%"SCNd64"\n", goldbach(&number));
+	if((number > 5 || number < 0)){
+      fprintf(output, "%"SCNd64"\n", goldbach(&number));
+    }else{
+      fprintf(output, "%"SCNd64 "%s", number,": NA\n");
+    }
+    
   }
-  
   return EXIT_SUCCESS;
 }
 
 int64_t goldbach(int64_t * number) {
-  int64_t goldbach= *number + *number;
+  int64_t goldbach= 0;
+  
+  //for(int )
   
   //int * primesVector = calculatePrimeNumbers();
   
@@ -28,3 +34,14 @@ int64_t goldbach(int64_t * number) {
   return goldbach;
 }
 
+int64_t * calculatePrimeNumbers(int num){
+  int64_t * primes = (int64_t*)malloc(sizeof(int64_t));
+  int primesSize = sizeof(primes)/sizeof(int64_t);
+  
+  for(int index = 0; index < primesSize; index++){
+    primes[index]=0;
+  }
+  
+  
+  return primes;
+}
