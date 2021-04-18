84f265d02c9c9c617e9d289c42a2ecedbe55a85f
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-18 14:34:19 -0600

    goldbach_serial: goldbach_sums_queue

---
+++ Tareas/goldbach_serial/goldbach_sums_queue.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "goldbach_sums_queue.h"
+
+
+
+/*int goldbach_sums_queue_init(goldbach_sums_queue* queue);
+void goldbach_sums_queue_insert(goldbach_sums_queue* queue, char num1, char num2);
+void goldbach_sums_queue_print(goldbach_sums_queue* queue);
+void goldbach_sums_queue_destroy(goldbach_sums_queue* queue);*/
