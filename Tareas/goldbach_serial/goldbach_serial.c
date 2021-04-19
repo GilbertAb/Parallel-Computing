af384805f9228d9cf6690ea5aa0f658c317cfc31
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-18 19:40:37 -0600

    goldbach_serial: fixing main andis_even_number, print_goldbach_sums added

--- Tareas/goldbach_serial/goldbach_serial.c
+++ Tareas/goldbach_serial/goldbach_serial.c
@@ -7,34 +7,58 @@
 
 #define MAXPRIMES 1000000
 
+int64_t * eratostenes_sieve(int64_t maxNum);
 int64_t* goldbach(int64_t * number, int64_t * primes);
 int64_t* goldbach_strong_conjecture(int64_t number, int64_t * primes);
 int64_t* goldbach_weak_conjecture(int64_t number, int64_t * primes);
-
-int64_t * eratostenes_sieve(int64_t maxNum);
+void * print_goldbach_sums(int64_t number, int64_t * goldbachSums);
+bool is_even_number(int64_t number);
 
 int main(void) {
-  FILE* input = stdin;
-  FILE* output = stdout;
   int64_t number = 0;
-  
   int64_t * primes = eratostenes_sieve(MAXPRIMES);
-  while(fscanf(input, "%"SCNd64, &number) == 1){
+  /*int64_t sizePrimes = sizeof(primes) / sizeof(int64_t);
+  printf("%"SCNd64"\n", sizePrimes);*/
+  while(scanf("%"SCNd64, &number) == 1){
 	if((number > 5 || number < 0)){
-      fprintf(output, "%"SCNd64"\n", number);
+      printf("%"SCNd64"\n", number);
+      /*if(number > primes[sizePrimes - 1]){
+        primes = eratostenes_sieve(number);
+      }*/
       int64_t * goldbach_sums = goldbach(&number, primes);
+      
     }else{
-      fprintf(output, "%"SCNd64 "%s", number,": NA\n");
+      printf("%"SCNd64 "%s", number,": NA\n");
     }
     
   }
   return EXIT_SUCCESS;
 }
 
+int64_t * eratostenes_sieve(int64_t maxNum) {   
+  //int64_t * primes = malloc(sizeof(int64_t) * MAXPRIMES);
+  int64_t * primes = calloc(MAXPRIMES, sizeof(int64_t));
+
+  int64_t amountPrimes = 0;
+  int64_t prime = 0;
+  int64_t nums[maxNum];
+
+  for(int index = 2;index <= maxNum; index++) {
+    if(nums[index] != 1 || index == 2) {
+      primes[amountPrimes] = index;
+      for(prime = 2;(prime * index) <= maxNum; prime++) {
+        nums[(prime * index)] = 1;
+      }
+      amountPrimes++;
+    }
+  }    
+  return primes;
+}
+
 int64_t* goldbach(int64_t * number, int64_t * primes) {
   int64_t* goldbach;
   
-  if(*number % 2 == 0){
+  if(/**number % 2 == 0*/ is_even_number(*number)){
     goldbach = goldbach_strong_conjecture(*number, primes);
   }else{
     goldbach = goldbach_weak_conjecture(*number, primes);
@@ -45,7 +69,7 @@ int64_t* goldbach(int64_t * number, int64_t * primes) {
   return goldbach;
 }
 
-//Para pares
+// Para pares
 int64_t* goldbach_strong_conjecture(int64_t number, int64_t * primes) {
   int64_t* a; 
   bool negative = false;
@@ -62,8 +86,7 @@ int64_t* goldbach_strong_conjecture(int64_t number, int64_t * primes) {
     for(int j = i; j < number; j++){
       if(primes[i] + primes[j] == number){
         ++amountSums;
-      }
-      
+      }      
     }  
   }
   printf("%"SCNd64 "%s" "%"SCNd64 "%s", number," sums: ", amountSums,"\n");
@@ -97,23 +120,29 @@ int64_t* goldbach_weak_conjecture(int64_t number, int64_t * primes) {
   return a;
 }
 
-int64_t * eratostenes_sieve(int64_t maxNum) {   
-  //int64_t * primes = malloc(sizeof(int64_t) * MAXPRIMES);
-  int64_t * primes = calloc(MAXPRIMES, sizeof(int64_t));
-
-  int64_t amountPrimes = 0;
-  int64_t prime = 0;
-  int64_t nums[maxNum];
-
-  for(int index = 2;index <= maxNum; index++) {
-    if(nums[index] != 1 || index == 2) {
-      primes[amountPrimes] = index;
-      for(prime = 2;(prime * index) <= maxNum; prime++) {
-        nums[(prime * index)] = 1;
-      }
-      amountPrimes++;
-    }
-  }  
+void * print_goldbach_sums(int64_t number, int64_t * goldbachSums){
+  int64_t size = 10;
+  int counter = 1;
+  int counterMax = 3;
   
-  return primes;
+  if(is_even_number(number)){
+    counterMax = 2;
+  }
+  
+  for(int index = 0; index < size; index++){
+      printf("%"SCNd64, goldbachSums[index]);
+      if(counter < counterMax){
+        printf("%s", " + ");
+        ++counter;
+      }else{
+        if(index + 1 != size){
+          printf("%s", ", ");
+          counter = 0;   
+	    } 
+      }
+  }
+}
+
+bool is_even_number(int64_t number){
+  return number % 2 == 0;
 }
