eafcab4b31534e3623e4dfbf624b62af06704cd2
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-04-20 00:22:58 -0600

    Hello_w.c making concurrence

--- Ejercicios_vistos_en_clase/hello_w/hello_w.c
+++ Ejercicios_vistos_en_clase/hello_w/hello_w.c
@@ -1,4 +1,5 @@
 // Copyright 2021 Gilbert Marquez Aldana <gilbert.marquez@ucr.ac.cr>
+// Creates an arbitrary amount of threads that greet in stdout
 
 #include <pthread.h>
 #include <stdio.h>
@@ -15,23 +16,29 @@ int main(int argc, char* argv[]) {
 	  return EXIT_FAILURE;
 	}
   }
-	
+
   pthread_t thread[thread_count];
   for (size_t index = 0; index < thread_count; ++index) {
     if (pthread_create(&thread[index], /*attr*/ NULL, run, (void*)index)
         == EXIT_SUCCESS) {
-      pthread_join(thread[index], /*value_ptr*/ NULL);
     } else {
       fprintf(stderr, "could not create secundary thread\n");
       return EXIT_FAILURE;
     }
-  } 
+  }
+  
   printf("Hello from main thread\n");
+  
+  for (size_t index = 0; index < thread_count; ++index) {
+    pthread_join(thread[index], /*value_ptr*/ NULL);
+  }
+
   return EXIT_SUCCESS;
 }
 
 void* run(void* data) {
   const size_t thread_number = (size_t)data;
   printf("Hello from secundary thread %zu\n", thread_number);
+
   return NULL;
 }
