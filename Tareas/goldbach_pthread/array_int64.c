d3c80e29ba8462930f07efaa6d867df5f06d0b66
GILBERT.MARQUEZ <gilbert.marquez@ucr.ac.cr>
2021-05-30 20:55:14 -0600

    concurrent Goldbach

---
+++ Tareas/goldbach_pthread/array_int64.c
@@ -0,0 +1,93 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <inttypes.h>//////////////////
+
+#include "array_int64.h"
+
+
+int array_int64_increase_capacity(array_int64_t* array);
+
+/**
+ * @brief initialize the array_int64 struct
+ * @details initialize the array_int64 struct
+ * @param array pointer to the array to be initialized
+ * @return returns an integer to check errors
+ */
+int array_int64_init(array_int64_t* array) {
+  assert(array);
+  array->capacity = 0;
+  array->count = 0;
+  array->elements = NULL;
+  return EXIT_SUCCESS;
+}
+/**
+ * @brief destroys the array_int64 struct
+ * @details initialize the array_int64 struct
+ * @param array pointer to the array to be destroyed
+ * @return returns an integer to check errors
+ */
+void array_int64_destroy(array_int64_t* array) {
+  assert(array);
+  array->capacity = 0;
+  array->count = 0;
+  free(array->elements);
+}
+
+/**
+ * @brief appends an element to the array_int64 struct
+ * @details appends an element to the array_int64 struct
+ * @param array pointer to the array
+ * @param element element to be Appended
+ * @return returns an integer to check errors
+ */
+int array_int64_append(array_int64_t* array, int64_t element) {
+  assert(array);
+  if (array->count == array->capacity) {
+    if (array_int64_increase_capacity(array) != EXIT_SUCCESS) {
+      return EXIT_FAILURE;
+    }
+  }
+  array->elements[array->count++] = element;
+  return EXIT_SUCCESS;
+}
+/**
+ * @brief increases the capacity of the array_int64 struct
+ * @details increases the capacity of the array_int64 struct by making it
+ * 10 times bigger
+ * @param array pointer to the array
+ * @return returns an integer to check errors
+ */
+int array_int64_increase_capacity(array_int64_t* array) {
+  int64_t new_capacity = 10 * (array->capacity ? array->capacity : 1);
+  int64_t* new_elements = (int64_t*)
+    realloc(array->elements, new_capacity * sizeof(int64_t));
+
+  if (new_elements) {
+    array->capacity = new_capacity;
+    array->elements = new_elements;
+    return EXIT_SUCCESS;
+  } else {
+    return EXIT_FAILURE;
+  }
+}
+
+/**
+ * @brief returns the count of elements in the array
+ * @details returns the count of elements in the array
+ * @param array pointer to the array
+ * @return returns the count of elements in the array
+ */
+int64_t array_int64_getCount(array_int64_t* array) {
+  return array->count;
+}
+
+/**
+ * @brief returns an element of the array
+ * @details returns an element of an specific position in the array 
+ * @param array pointer to the array
+ * @param position position of the element in the array
+ * @return returns an element of the array
+ */
+int64_t array_int64_getElement(array_int64_t* array, int64_t position) {
+  return array->elements[position];
+}
---
+++ Tareas/goldbach_pthread/goldbach_serial.c
@@ -0,0 +1,224 @@
+// Copyright 2021 Gilbert Marquez Aldana <gilbert.marquez@ucr.ac.cr>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array_int64.h"
+
+int goldbach(int64_t number, array_int64_t * goldbach_sums);
+int goldbach_strong_conjecture(int64_t number, array_int64_t * goldbach_sums);
+int goldbach_weak_conjecture(int64_t number, array_int64_t * goldbach_sums);
+void print_goldbach_sums(int64_t number, array_int64_t * goldbachSums);
+bool is_even_number(int64_t number);
+bool isPrime(int64_t number);
+
+/**
+ * @return zero if succeed
+ */
+int main(void) {
+  int64_t error = EXIT_SUCCESS;
+  int64_t number = 0;
+  int64_t amountGoldbachSums = 0;
+
+  while (scanf("%"SCNd64, &number) == 1) {
+    bool negativeInput = number < 0 ? true : false;
+    
+    if(negativeInput){
+      number *= -1;
+    }
+    
+    if (number < 0 || number > 5) {
+      if (negativeInput) {
+        printf("%"SCNd64, -number);
+      }else{
+        printf("%"SCNd64, number);
+      }
+      array_int64_t goldbach_sums;
+      error = array_int64_init(&goldbach_sums);
+      if (error == EXIT_SUCCESS) {
+        error = goldbach(number, &goldbach_sums);
+        if (error == EXIT_SUCCESS) {
+          if (is_even_number(number)) {
+            amountGoldbachSums = array_int64_getCount(&goldbach_sums) / 2;
+          } else {
+            amountGoldbachSums = array_int64_getCount(&goldbach_sums) / 3;
+          }
+      
+          printf("%s""%"SCNd64"%s", ": " , amountGoldbachSums, " sums");
+          if (negativeInput){
+            printf("%s", ": ");
+            print_goldbach_sums(number, &goldbach_sums);
+          }
+          printf("%s", "\n");
+      
+          array_int64_destroy(&goldbach_sums);
+        } else {
+          fprintf(stderr, "error: could not calculate goldbach sums\n");
+        }
+      } else {
+        fprintf(stderr, "error: could not init goldbach_sums.\n");
+      }
+    } else {
+      if (negativeInput) {
+        printf("%"SCNd64 "%s", -number, ": NA\n");
+      }else{
+        printf("%"SCNd64 "%s", number, ": NA\n");
+      }
+    }
+  }
+
+  return error;
+}
+
+/**
+ * @brief Constructs an array with the goldbach sums
+ * @details Verifies if the number is even or odd, then calls
+ * a conjecture to create the array of goldbach sums
+ * @param number The number wich goldbach sums will be find
+ * @param goldbach_sums pointer to the array with the goldbach sums
+ * @return Returns an integer to check errors
+ */
+int goldbach(int64_t number, array_int64_t * goldbach_sums) {
+  int error = EXIT_SUCCESS;
+
+  if (is_even_number(number)){
+    error = goldbach_strong_conjecture(number, goldbach_sums);
+  } else {
+    error = goldbach_weak_conjecture(number, goldbach_sums);
+  }
+
+  return error;
+}
+
+/**
+ * @brief Constructs an array with the goldbach sums
+ * @details The conjecture for even numbers, the first position of the array
+ * is the amount of goldbach sums of the number. the next positions are the
+ * numbers that conform the sums (they will be accessed in pairs to print)
+ * @param number The number wich goldbach sums will be find
+ * @param goldbach_sums pointer to the array with the goldbach sums
+ * @return Returns an integer to check errors
+ */
+int goldbach_strong_conjecture(int64_t number, array_int64_t * goldbach_sums) {
+  int error = EXIT_SUCCESS;
+
+  for (int64_t num1 = 2; num1 < number && !error; ++num1) {
+    if (isPrime(num1)) {
+      for (int64_t num2 = num1; num2 < number; ++num2){
+        if (num1 + num2 == number && isPrime(num2)) {
+          error = array_int64_append(goldbach_sums, num1);
+          if (error) {
+            break;
+          }
+          error = array_int64_append(goldbach_sums, num2);
+          if (error) {
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  return error;
+}
+
+/**
+ * @brief Constructs an array with the goldbach sums
+ * @details The conjecture for odd numbers, the first position of the array
+ * is the amount of goldbach sums of the number. the next positions are the
+ * numbers that conform the sums (they will be accessed int trios to print)
+ * @param number The number wich goldbach sums will be find
+ * @param goldbach_sums pointer to the array with the goldbach sums
+ * @return Returns an integer to check errors
+ */
+int goldbach_weak_conjecture(int64_t number, array_int64_t * goldbach_sums){
+  int error = EXIT_SUCCESS;
+  
+  for (int64_t num1 = 2; num1 < number && !error; ++num1) {
+    if (isPrime(num1)) {
+      for (int64_t num2 = num1; num2 < number && !error; ++num2) {
+        if (isPrime(num2)) {
+          for (int64_t num3 = num2; num3 < number; ++num3) {
+            if (num1 + num2 + num3 == number && isPrime(num3) ) {
+              error = array_int64_append(goldbach_sums, num1);
+              if (error) {
+                break;
+              }
+              error = array_int64_append(goldbach_sums, num2);
+              if (error) {
+                break;
+              }
+              error = array_int64_append(goldbach_sums, num3);
+              if (error) {
+                break;
+              }
+            }
+          }
+        }
+      }
+    }  
+  }
+  
+  return error;
+}
+
+/**
+ * @brief Prints the goldbach sums of a number
+ * @details Prints the goldbach sums of a number
+ * @param number The number wich goldbach sums will be printed
+ * @param goldbach_sums pointer to the array with the goldbach sums
+ */
+void print_goldbach_sums(int64_t number, array_int64_t * goldbach_sums) {
+  int counter = 1;
+  int counterMax = 3;
+  int index = 0;
+
+  if (is_even_number(number)) {
+    counterMax = 2;
+  }
+
+  while (index < array_int64_getCount(goldbach_sums)) {
+    printf("%"SCNd64, array_int64_getElement(goldbach_sums, index));
+    if (counter < counterMax) {
+      printf("%s", " + ");
+      ++counter;
+    } else {
+      if ((index + 1) != array_int64_getCount(goldbach_sums)) {
+        printf("%s", ", ");
+      }
+      counter = 1;
+    }
+    index++;
+  }
+}
+
+/**
+ * @brief Returns if a number is even
+ * @details proves if a number is divisible by 2
+ * @param number The number
+ * @return Returns true if a number is even
+ */
+bool is_even_number(int64_t number) {
+  return number % 2 == 0;
+}
+
+/**
+ * @brief Returns if a number is a prime number
+ * @details Determines if a number is a prime number by checking if it
+ * has more than two divisors
+ * @param number The number
+ * @return Returns true if a number is a prime number
+ */
+bool isPrime(int64_t number) {
+  bool isPrime = true;
+
+  for (int64_t i = 2; i < number; ++i){
+    if (number % i == 0) {
+      isPrime = false;
+    }
+  }  
+ 
+  return isPrime;
+}
